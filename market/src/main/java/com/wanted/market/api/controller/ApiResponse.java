cefb723114400a30caa8bf54d88e446c6a8e64cf
yeri <leeyr0412@naver.com>
2024-06-02 00:55:57 +0900

    :recycle: refactor: ApiResponse 변경

--- market/src/main/java/com/wanted/market/api/controller/ApiResponse.java
+++ market/src/main/java/com/wanted/market/api/controller/ApiResponse.java
@@ -5,23 +5,25 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse<T> {
+    private final boolean result;
     private final int code;
     private final HttpStatus status;
     private final String message;
     private final T data;
 
-    private ApiResponse(HttpStatus status, String message, T data) {
+    private ApiResponse(boolean result, HttpStatus status, String message, T data) {
+        this.result = result;
         this.code = status.value();
         this.status = status;
         this.message = message;
         this.data = data;
     }
 
-    public static <T> ApiResponse<T> of(T data) {
-        return new ApiResponse<>(HttpStatus.BAD_REQUEST, "Error", data);
+    public static <T> ApiResponse<T> of(HttpStatus status, String message) {
+        return new ApiResponse<>(false, status, message, null);
     }
 
     public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(HttpStatus.OK, "Success", data);
+        return new ApiResponse<>(true, HttpStatus.OK, "Success", data);
     }
 }
